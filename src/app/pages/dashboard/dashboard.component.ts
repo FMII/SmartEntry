55cd73ff8b4fd4900493014ed5a75253f89d8e47
FMII <ivettemolina.009@gmail.com>
2025-06-30 16:22:49 -0600

    logout

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
-
+  constructor(private authService: AuthService, private router: Router) { }
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/login']); // Redirige al login
+  }
 }
