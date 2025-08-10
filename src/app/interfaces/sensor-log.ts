209e8ce42bbd99bc0f6fcf35b146f44217f25e06
misrael97 <moromoroni123@gmail.com>
2025-08-10 14:46:19 -0600

    filtro y funcionamiento tabla sensores

--- src/app/interfaces/sensor-log.ts
+++ src/app/interfaces/sensor-log.ts
@@ -5,10 +5,43 @@ export interface SensorLogResponse {
 }
 
 export interface SensorLogEntry {
-  sensor_name: string;
-  date: string;
-  user_name: string;
-  classroom_name?: string;
-  message: string;
+  id: number;
+  sensor_id: number;
+  response: {
+    msg: string;
+    data: {
+      accessLog?: {
+        id: number;
+        user_id: number;
+        sensor_id: number;
+        access_time: string;
+        classroom_id: number;
+      };
+      shift?: {
+        id: number;
+        date: string;
+        user_id: number;
+        sensor_id: number;
+        check_in_time: string;
+        check_out_time: string | null;
+      };
+    };
+    status: string;
+  };
+  created_at: string;
+  sensors: {
+    name: string;
+  };
 }
+
+// Interfaz para mapear los datos mostrados en la tabla
+export interface SensorLogDisplay {
+  sensorName: string;
+  date: string;
+  userName: string;
+  classroomName: string;
+  message: string;
+  status: string;
+}
+
 
